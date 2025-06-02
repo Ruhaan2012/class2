95c5f0fc0e407fa6b3f0a03fd75f2e15fa57462d
Ruhaan2012 <kpspahwa@yahoo.com>
2025-06-02 18:43:53 +1000

    Add files via upload

---
+++ class2.py
@@ -0,0 +1,21 @@
+#activity1
+"""print("welcome to the world of programing \n")
+print(5)"""
+
+#activity2
+name="jhon"
+print("my friend name is ",name)
+x=12
+print("my age is ",x)
+
+new="green"
+print("my favourite colour is ",new)
+
+#activity3
+import keyword
+print(keyword.kwlist)
+print("my name is ",name,"i am",x,"years old")
+
+z="harrison school"
+u="ruhaan" 
+print("my name is ",u,"i am studying at ",z)
