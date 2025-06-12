7e10ab7e35846fece54138715b50628b6894bb97
Ruhaan2012 <kpspahwa@yahoo.com>
2025-06-12 19:46:36 +1000

    Add files via upload

---
+++ class4.py
@@ -0,0 +1,28 @@
+"""There are five trees in Jack's front yard. He checks each tree to find out how tall it is in inches
+ and writes the height on a sheet of paper.
+   Jack's list: 98, 94, 41, 96, and 11. What is the average height of a tree in Jack's front yard?
+tree1=98
+tree2=94
+tree3=41
+tree4=96
+tree5=11
+sum=tree1+tree2+tree3+tree4+tree5
+print("the sum  of all these numbers are ",sum)
+average=sum/5
+print("average= ",average)"""
+
+"""Raj scored 40, 70, 50 and 60 out of 100 in maths, science, Hindi and English. Find the percentage he got?
+totalmark=400
+science=int(input("enter science marks: "))
+maths=int(input("enter maths marks: "))
+hindi=int(input("enter hindi marks: "))
+english=int(input("enter english marks: "))
+sum=science+maths+hindi+english
+print("sum of all scores equal ",sum)
+precent=(sum/totalmark)*100
+print("precentage equal ",precent)"""
+
+print(2**3)
+print(15**2)
+print(8**4)
+print(16**0.5)
