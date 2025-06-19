f3dcc515934a51e48a6365e192716d10a5ad534c
Ruhaan2012 <kpspahwa@yahoo.com>
2025-06-19 18:57:54 +1000

    Add files via upload

---
+++ project2.py
@@ -0,0 +1,17 @@
+birthday="4 of june"
+print("i am born on the ",birthday)
+
+birthday2="5 of june"
+print("i am born on the ",birthday2)
+
+birthday3="6 of june"
+print("i am born on the ",birthday3)
+
+birthday4="9 of june"
+print("i am born on the ",birthday4)
+
+birthday5="8 of june"
+print("i am born on the ",birthday5)
+
+
+
