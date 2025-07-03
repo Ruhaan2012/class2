572ac6b061ce04e36e1577def75e671751582789
Ruhaan2012 <kpspahwa@yahoo.com>
2025-07-03 19:41:30 +1000

    Add files via upload

---
+++ c10.py
@@ -0,0 +1,27 @@
+"""Sum of whole numbers
+Outline:
+Write a program to calculate the sum of whole numbers.
+
+Reverse a String
+Outline:
+Write a program to reverse the string entered by the user.
+
+reverse order
+Outline:
+Write a program to print the numbers in reverse order beginning from the number entered by the user."""
+
+#number=int(input("enter the number whoes sum you want to find "))
+#sum=0
+#for i in range(1,number+1):
+ #   sum=sum+i
+#print(sum)
+
+#n=int(input("enter the values "))
+#for i in range(n,0,-1):
+#    print(i)
+
+name=input("please enter your name ")
+name2=("")
+for i in name:
+    name2=i+name2
+print(name2)
