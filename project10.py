6425dda58865f7e3ccf044e3e312ea4ab15b7c02
Ruhaan2012 <kpspahwa@yahoo.com>
2025-07-10 17:52:07 +1000

    Add files via upload

---
+++ project10.py
@@ -0,0 +1,9 @@
+while True: 
+    number1=int(input("enter number 1"))
+    number2=int(input("enter number 2"))
+
+    print("addition = to",number1+number2)
+    print("subtracion = to",number1-number2)
+    print("multiplacation = to",number1*number2)
+    print("division = to",number1/number2)
+    
