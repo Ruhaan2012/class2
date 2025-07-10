229d0bc26bee9318a7df041be5fd664b4381b94e
Ruhaan2012 <kpspahwa@yahoo.com>
2025-07-10 18:14:04 +1000

    Add files via upload

---
+++ pojrct8.py
@@ -0,0 +1,8 @@
+num2 = int(input("Enter your age: "))
+
+if num2 > 20:
+   print("you are not allowed in the class ")
+elif num2 <= 20:
+   print("you are allowed in the class ")
+else:
+   print("you are not allowed in class")
